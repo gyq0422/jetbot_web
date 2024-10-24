26e7ab09bc5a016fc7ae1df60ff7b9f627b7470b
dirosm <13832696+dirosm@user.noreply.gitee.com>
2024-10-25 02:50:52 +0800

    Merge remote-tracking branch 'origin/master'

--- app.py
+++ app.py
@@ -1,66 +1,55 @@
-# -*- coding: utf-8 -*-
-from importlib import import_module
-import os
-from flask import Response
-
-
-# import camera driver
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-else:
-    from camera import Camera
-
-# Raspberry Pi camera module (requires picamera package)
-# from camera_pi import Camera
-from teleop_twist_keyboard import handle_command, PublishThread
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from pynput.keyboard import Controller, Key
 import threading
-import rospy
-from geometry_msgs.msg import Twist
+import os
 
-# 初始化 Flask 应用和 SocketIO
+# 创建Flask应用和SocketIO对象
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# 初始化 ROS 节点和 Publisher
-rospy.init_node('web_teleop_twist_keyboard', anonymous=True)
+# 创建键盘控制器对象
+keyboard = Controller()
 
-speed = rospy.get_param("~speed", 0.5)
-turn = rospy.get_param("~turn", 1.0)
-speed_limit = rospy.get_param("~speed_limit", 1000)
-turn_limit = rospy.get_param("~turn_limit", 1000)
-repeat = rospy.get_param("~repeat_rate", 0.0)
+# 启动键盘控制ROS脚本
+def start_jetbot_keyboard():
+    os.system("roslaunch jetbot_ros jetbotmini_keyboard.launch")
 
-pub_thread = PublishThread(repeat)
+# 在一个新线程中启动键盘控制脚本，以免阻塞Flask服务器
+threading.Thread(target=start_jetbot_keyboard, daemon=True).start()
 
+# 主页
 @app.route('/')
-def gui():
+def index():
     return render_template('gui.html')
 
-
-@socketio.on('command')
-def handle_command_from_web(data):
-    global speed, turn
-    command = data['command']
-    speed, turn = handle_command(command, pub_thread, speed, turn, speed_limit, turn_limit)
-
-   
-
-def gen(camera):
-
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
+# 处理前端的控制命令
+@socketio.on('move_command')
+def handle_move_command(command):
+    if command == 'forward':
+        keyboard.press('w')
+        keyboard.release('w')
+    elif command == 'backward':
+        keyboard.press('s')
+        keyboard.release('s')
+    elif command == 'left':
+        keyboard.press('a')
+        keyboard.release('a')
+    elif command == 'right':
+        keyboard.press('d')
+        keyboard.release('d')
+    elif command == 'speed_left_increase':
+        keyboard.press('u')
+        keyboard.release('u')
+    elif command == 'speed_left_decrease':
+        keyboard.press('i')
+        keyboard.release('i')
+    elif command == 'speed_right_increase':
+        keyboard.press('j')
+        keyboard.release('j')
+    elif command == 'speed_right_decrease':
+        keyboard.press('k')
+        keyboard.release('k')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
--- static/js/gui.js
+++ static/js/gui.js
@@ -1,100 +1,23 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const socket = io.connect('http://localhost:5000');
+// JavaScript 文件: script.js
+$(document).ready(function () {
+    // 绑定按钮点击事件
+    $('.control-button').on('click', function () {
+        var command = $(this).attr('id');
+        sendCommand(command);
+    });
 
+    // 发送控制命令到服务器的函数
     function sendCommand(command) {
-        socket.emit('command', { 'command': command });
-        console.log("Command sent: " + command);
+        $.ajax({
+            url: '/control',
+            type: 'POST',
+            data: { command: command },
+            success: function (response) {
+                console.log('Command sent: ' + command);
+            },
+            error: function (error) {
+                console.error('Error sending command: ' + command);
+            }
+        });
     }
-
-    // 按钮点击事件处理
-    document.getElementById('forward').addEventListener('click', () => {
-        sendCommand('i');  // 对应前进
-    });
-
-    document.getElementById('backward').addEventListener('click', () => {
-        sendCommand(',');  // 对应后退
-    });
-
-    document.getElementById('left').addEventListener('click', () => {
-        sendCommand('j');  // 对应左转
-    });
-
-    document.getElementById('right').addEventListener('click', () => {
-        sendCommand('l');  // 对应右转
-    });
-
-    document.getElementById('stop').addEventListener('click', () => {
-        sendCommand(' ');  // 停止
-    });
-
-    // 键盘输入事件处理
-    window.addEventListener('keydown', (event) => {
-        let key = event.key;
-        let command = "";
-
-        // 匹配原有键盘控制
-        switch (key) {
-            case "ArrowUp":
-            case "w":
-                command = 'i';  // 前进
-                break;
-            case "ArrowDown":
-            case "s":
-                command = ',';  // 后退
-                break;
-            case "ArrowLeft":
-            case "a":
-                command = 'j';  // 左转
-                break;
-            case "ArrowRight":
-            case "d":
-                command = 'l';  // 右转
-                break;
-            case "u":
-                command = 'u';  // 前进 + 左转
-                break;
-            case "o":
-                command = 'o';  // 前进 + 右转
-                break;
-            case "m":
-                command = 'm';  // 后退 + 左转
-                break;
-            case ".":
-                command = '.';  // 后退 + 右转
-                break;
-            case "U":
-                command = 'U';  // 高级模式 前进 + 左转
-                break;
-            case "I":
-                command = 'I';  // 高级模式 前进
-                break;
-            case "O":
-                command = 'O';  // 高级模式 前进 + 右转
-                break;
-            case "J":
-                command = 'J';  // 高级模式 左移
-                break;
-            case "L":
-                command = 'L';  // 高级模式 右移
-                break;
-            case "M":
-                command = 'M';  // 高级模式 后退 + 左移
-                break;
-            case "<":
-                command = '<';  // 高级模式 后退
-                break;
-            case ">":
-                command = '>';  // 高级模式 后退 + 右移
-                break;
-            case " ":
-                command = ' ';  // 停止
-                break;
-            default:
-                command = "";
-        }
-
-        if (command) {
-            sendCommand(command);
-        }
-    });
 });
